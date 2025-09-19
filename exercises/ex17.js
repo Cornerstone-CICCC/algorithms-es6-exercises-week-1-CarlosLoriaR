90c7d15116909c01849fcb990f2d131deb10823a
Carlos Loría Rodríguez <carlosloriar5@gmail.com>
2025-09-18 20:32:15 -0700

    exercise 17 completed

--- exercises/ex17.js
+++ exercises/ex17.js
@@ -4,3 +4,12 @@ Write an anonymous function expression that stores a function in a variable call
 laugh(3);
 Returns: hahaha!
 */
+const laugh = function (number) {
+    let message = ""
+    for (let x = 0; x < number; x++) {
+        message += "ha"
+    }
+    message += "!"
+    console.log(message)
+}
+laugh(3)
