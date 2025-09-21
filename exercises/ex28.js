74e6cba2adef15161e58e38debac87d840c2750b
Carlos Loría Rodríguez <carlosloriar5@gmail.com>
2025-09-20 21:39:56 -0700

    exercise 28 completed

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,28 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+let facebookProfile = {
+    name: "Carlos Loria",
+    numberOfFriends: 300,
+    messagesPosted: ["Vacation mode activated", "Happy silk song release day", "I really want to go to lost lands"],
+    postMessage: function postMessage(message) {
+        this.messagesPosted.push(message)
+        return this.messagesPosted
+    },
+    deleteMessage: function deleteMessage(index) {
+        this.messagesPosted.splice(index, 1)
+        return this.messagesPosted
+    },
+    addFriend: function addFriend() {
+        this.numberOfFriends++
+        return this.numberOfFriends
+    },
+    removeFriend: function removeFriend() {
+        this.numberOfFriends--
+        return this.numberOfFriends
+    }
+}
+console.log(facebookProfile.postMessage("Test"));
+console.log(facebookProfile.deleteMessage(1));
+console.log(facebookProfile.addFriend());
+console.log(facebookProfile.removeFriend());
